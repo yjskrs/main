b83a875ef49abc1bfd52984103385e5a2e6c00a2
waynewee <wwayne.wee@gmail.com>
2020-03-14 20:59:27 +0800

    hotfix for unresponsive avatar display

--- src/main/java/igrad/ui/AvatarSelectionPanel.java
+++ src/main/java/igrad/ui/AvatarSelectionPanel.java
@@ -6,11 +6,13 @@ import java.util.logging.Logger;
 import igrad.commons.core.LogsCenter;
 import igrad.model.avatar.Avatar;
 import javafx.fxml.FXML;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Region;
 import javafx.scene.text.Font;
+import javafx.stage.Screen;
 
 /**
  * Panel containing the list of modules.
@@ -26,6 +28,7 @@ public class AvatarSelectionPanel extends UiPart<Region> {
     private Label avatarLabel;
 
     private List<AvatarImage> avatarImgList = new ArrayList<>();
+    private Rectangle2D primaryScreenBounds = Screen.getPrimary().getVisualBounds();
 
     public AvatarSelectionPanel() {
         super(FXML);
@@ -63,8 +66,8 @@ public class AvatarSelectionPanel extends UiPart<Region> {
     private void showAvatarImgList() {
         for (AvatarImage avatarImg : avatarImgList) {
             ImageView avatarDisplay = new ImageView();
-            avatarDisplay.setFitHeight(250.0);
-            avatarDisplay.setFitWidth(250.0);
+            avatarDisplay.setFitHeight(0.1 * primaryScreenBounds.getHeight());
+            avatarDisplay.setFitWidth(0.1 * primaryScreenBounds.getHeight());
             avatarDisplay.setImage(avatarImg);
             GridPane.setRowIndex(avatarDisplay, avatarImg.getRowIndex());
             GridPane.setColumnIndex(avatarDisplay, avatarImg.getColIndex());
