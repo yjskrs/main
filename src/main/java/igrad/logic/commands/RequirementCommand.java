d6e0af952985871aed9366c3a2abe0dfa7509ac5
yjskrs <e0323600@u.nus.edu>
2020-03-17 16:45:12 +0800

    Add RequirementCommand class

---
+++ src/main/java/igrad/logic/commands/RequirementCommand.java
@@ -0,0 +1,8 @@
+package igrad.logic.commands;
+
+/**
+ * A generic Requirement class.
+ */
+public abstract class RequirementCommand extends Command {
+    public static final String REQUIREMENT_COMMAND_WORD = "requirement ";
+}
