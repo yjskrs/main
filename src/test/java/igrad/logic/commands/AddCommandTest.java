1babbcc89485ad372f2497e19ec37144b7a27d67
waynewee <wwayne.wee@gmail.com>
2020-03-19 21:07:17 +0800

    fixed implementation error of modelstub

--- src/test/java/igrad/logic/commands/AddCommandTest.java
+++ src/test/java/igrad/logic/commands/AddCommandTest.java
@@ -109,6 +109,11 @@ public class AddCommandTest {
         }
 
         @Override
+        public Path getBackupCourseBookFilePath() {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
         public void setCourseBookFilePath(Path courseBookFilePath) {
             throw new AssertionError("This method should not be called.");
         }
