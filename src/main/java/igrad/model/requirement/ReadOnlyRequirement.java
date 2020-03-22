1ffce71a49faea64669594a3a75726fb9235c500
NinoMii <e0323600@u.nus.edu>
2020-03-22 22:00:16 +0800

    Update requirement class with credit methods

--- src/main/java/igrad/model/requirement/ReadOnlyRequirement.java
+++ src/main/java/igrad/model/requirement/ReadOnlyRequirement.java
@@ -19,9 +19,25 @@ public interface ReadOnlyRequirement {
     Credits getCredits();
 
     /**
+     * Returns the credits required for the requirement.
+     */
+    String getCreditsRequired();
+
+    /**
+     * Returns the credits fulfilled for the requirement.
+     */
+    String getCreditsFulfilled();
+
+    /**
      * Returns an unmodifiable view of the modules in this requirement.
      * This list will not contain any duplicate modules.
      */
     ObservableList<Module> getModuleList();
+
+    /**
+     * Returns a boolean value to indicate whether the requirement is fulfilled.
+     * Returns true if requirement is fulfilled (credits are fulfilled), else return false.
+     */
+    boolean isFulfilled();
 
 }
