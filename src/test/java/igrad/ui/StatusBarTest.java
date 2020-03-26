aa11c4b96ad36261f064ecb1717d78b93a8f5691
Daryl Goh <dargohzy@users.noreply.github.com>
2020-03-27 03:07:14 +0800

    Fix checkstyle

--- src/test/java/igrad/ui/StatusBarTest.java
+++ src/test/java/igrad/ui/StatusBarTest.java
@@ -1,10 +1,10 @@
 package igrad.ui;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+/*import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test;*/
 
 public class StatusBarTest {
 
