410c1a313505f714c1feee0395bf55eb978e23a5
NinoMii <e0323600@u.nus.edu>
2020-04-01 13:24:45 +0800

    Fix messages

--- src/main/java/igrad/commons/core/Messages.java
+++ src/main/java/igrad/commons/core/Messages.java
@@ -5,20 +5,20 @@ package igrad.commons.core;
  */
 public class Messages {
 
-    public static final String MESSAGE_UNKNOWN_COMMAND = "%1$s does not know this command, "
+    public static final String MESSAGE_UNKNOWN_COMMAND = "I don't know this command, "
         + "you may key in `help` to get a list of commands!";
-    public static final String MESSAGE_UNKNOWN_COURSE_COMMAND = "%1$s does not know this course command, "
+    public static final String MESSAGE_UNKNOWN_COURSE_COMMAND = "I don't know this course command, "
         + "you might want to try:\n"
         + "course add ...\n"
         + "course edit ...\n"
         + "course delete\n";
-    public static final String MESSAGE_UNKNOWN_REQUIREMENT_COMMAND = "%1$s does not know this requirement command, "
+    public static final String MESSAGE_UNKNOWN_REQUIREMENT_COMMAND = "I don't know this requirement command, "
         + "you might want to try:\n"
         + "requirement add ...\n"
         + "requirement edit ...\n"
         + "requirement delete ...\n"
         + "requirement assign ...\n";
-    public static final String MESSAGE_UNKNOWN_MODULE_COMMAND = "%1$s does not know this module command, "
+    public static final String MESSAGE_UNKNOWN_MODULE_COMMAND = "I don't know this module command, "
         + "you might want to try:\n"
         + "module add ...\n"
         + "module edit ...\n"
@@ -27,7 +27,7 @@ public class Messages {
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format!\n%1$s";
     public static final String MESSAGE_COURSE_NOT_SET = "You need to set a course first! Use this command:\n"
         + "course add n/COURSE_NAME";
-    public static final String MESSAGE_COURSE_ALREADY_SET = "Course has been set! Only one course can be added";
+    public static final String MESSAGE_COURSE_ALREADY_SET = "Course has been set! Only one course can be added.";
     public static final String MESSAGE_SPECIFIER_NOT_SPECIFIED = "Please provide a non-empty specifier.\n%1$s";
     public static final String MESSAGE_SPECIFIER_INVALID = "Please enter a valid specifier.\n%1$s";
 
