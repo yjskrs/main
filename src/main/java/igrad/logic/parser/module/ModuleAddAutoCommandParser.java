b5caded3a3324593ddec75fc65de54d7b1441686
Nathanael Seen <e0345677@u.nus.edu>
2020-04-07 14:43:32 +0800

    Fix minor ui bug with requirement fulfilled

--- src/main/java/igrad/logic/parser/module/ModuleAddAutoCommandParser.java
+++ src/main/java/igrad/logic/parser/module/ModuleAddAutoCommandParser.java
@@ -2,12 +2,7 @@ package igrad.logic.parser.module;
 
 import static igrad.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static igrad.commons.core.Messages.MESSAGE_REQUEST_FAILED;
-import static igrad.logic.parser.CliSyntax.PREFIX_CREDITS;
-import static igrad.logic.parser.CliSyntax.PREFIX_DESCRIPTION;
 import static igrad.logic.parser.CliSyntax.PREFIX_MODULE_CODE;
-import static igrad.logic.parser.CliSyntax.PREFIX_SEMESTER;
-import static igrad.logic.parser.CliSyntax.PREFIX_TAG;
-import static igrad.logic.parser.CliSyntax.PREFIX_TITLE;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -22,11 +17,8 @@ import igrad.logic.parser.Parser;
 import igrad.logic.parser.Prefix;
 import igrad.logic.parser.exceptions.ParseException;
 import igrad.model.module.Credits;
-import igrad.model.module.Description;
-import igrad.model.module.Grade;
 import igrad.model.module.Module;
 import igrad.model.module.ModuleCode;
-import igrad.model.module.Semester;
 import igrad.model.module.Title;
 import igrad.services.JsonParsedModule;
 import igrad.services.NusModsRequester;
