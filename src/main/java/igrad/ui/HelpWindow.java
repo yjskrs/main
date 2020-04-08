6fe3d78031ef6671055125233b5df6c8df1c741e
Daryl Goh <dargohzy@users.noreply.github.com>
2020-04-08 23:37:46 +0800

    Fixed HelpWindow link

--- src/main/java/igrad/ui/HelpWindow.java
+++ src/main/java/igrad/ui/HelpWindow.java
@@ -19,7 +19,7 @@ import javafx.stage.Stage;
  */
 public class HelpWindow extends UiPart<Stage> {
 
-    public static final String USERGUIDE_URL = "https://ay1920s2-cs2103t-f09-3.github.io/main/";
+    public static final String USERGUIDE_URL = "https://ay1920s2-cs2103t-f09-3.github.io/main/UserGuide.html";
 
     private static final Logger logger = LogsCenter.getLogger(HelpWindow.class);
     private static final String FXML = "HelpWindow.fxml";
