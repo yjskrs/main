2393f1b6986e963a797c654a4b02ec9e67bdfda8
Nathanael Seen <e0345677@u.nus.edu>
2020-04-11 09:56:48 +0800

    Complete constructor tests  for ModuleDoneCommand

--- src/test/java/igrad/logic/commands/module/ModuleDoneCommandTest.java
+++ src/test/java/igrad/logic/commands/module/ModuleDoneCommandTest.java
@@ -2,10 +2,20 @@ package igrad.logic.commands.module;
 
 //@@author nathanaelseen
 
-// import org.junit.jupiter.api.Test;
+import static igrad.logic.commands.module.ModuleCommandTestUtil.VALID_MODULE_CODE_CS1101S;
+import static igrad.testutil.Assert.assertThrows;
+// import static org.junit.jupiter.api.Assertions.assertFalse;
+// import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.Test;
+
+import igrad.logic.commands.module.ModuleDoneCommand.EditModuleDescriptor;
 import igrad.model.Model;
 import igrad.model.ModelManager;
+// import igrad.model.module.Module;
+import igrad.model.module.ModuleCode;
+import igrad.testutil.EditModuleDescriptorBuilder2;
+// import igrad.testutil.ModuleBuilder;
 
 
 /**
@@ -14,4 +24,20 @@ import igrad.model.ModelManager;
  */
 public class ModuleDoneCommandTest {
     private Model model = new ModelManager();
+
+    @Test
+    public void constructor_null_throwsNullPointerException() {
+        // ModuleCode null, but EditModuleDescriptor not null
+        ModuleCode moduleCode1 = null;
+        EditModuleDescriptor descriptor1 = new EditModuleDescriptorBuilder2()
+            .build();
+        assertThrows(NullPointerException.class, (
+                    ) -> new ModuleDoneCommand(moduleCode1, descriptor1));
+
+        // EditModuleDescriptor null, but ModuleCode not null
+        ModuleCode moduleCode2 = new ModuleCode(VALID_MODULE_CODE_CS1101S);
+        EditModuleDescriptor descriptor2 = null;
+        assertThrows(NullPointerException.class, (
+                    ) -> new ModuleDoneCommand(moduleCode2, descriptor2));
+    }
 }
