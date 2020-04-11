0b4b9ffffdd499a717522840809b96513df9a0c6
Nathanael Seen <e0345677@u.nus.edu>
2020-04-11 16:51:37 +0800

    Complete tests for model null execute method RequirementAssignCommand.java

--- src/test/java/igrad/logic/commands/requirement/RequirementAssignCommandTest.java
+++ src/test/java/igrad/logic/commands/requirement/RequirementAssignCommandTest.java
@@ -1,5 +1,7 @@
 package igrad.logic.commands.requirement;
 
+//@@author nathanaelseen
+
 import static igrad.logic.commands.module.ModuleCommandTestUtil.VALID_MODULE_CODE_CS1101S;
 import static igrad.logic.commands.module.ModuleCommandTestUtil.VALID_MODULE_CODE_CS2040;
 import static igrad.logic.commands.module.ModuleCommandTestUtil.VALID_MODULE_CODE_CS2100;
@@ -11,9 +13,11 @@ import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
+import igrad.model.Model;
 import igrad.model.module.ModuleCode;
 import igrad.model.requirement.RequirementCode;
 // import igrad.model.requirement.Requirement;
+// import igrad.model.ModelManager;
 
 public class RequirementAssignCommandTest {
     @Test
@@ -37,5 +41,21 @@ public class RequirementAssignCommandTest {
 
         assertThrows(NullPointerException.class, (
         ) -> new RequirementAssignCommand(requirementCodeB, moduleCodesB));
+    }
+
+    @Test
+    public void execute_nullModel_throwsNullPointerException() {
+        Model model = null;
+        RequirementCode requirementCode = new RequirementCode(VALID_REQ_CODE_UE);
+        List<ModuleCode> moduleCodes = new ArrayList<ModuleCode>() {
+            {
+                add(new ModuleCode(VALID_MODULE_CODE_CS1101S));
+                add(new ModuleCode(VALID_MODULE_CODE_CS2040));
+                add(new ModuleCode(VALID_MODULE_CODE_CS2100));
+            }
+        };
+
+        RequirementAssignCommand cmd = new RequirementAssignCommand(requirementCode, moduleCodes);
+        assertThrows(NullPointerException.class, () -> cmd.execute(model));
     }
 }
