54266e041d01931e8310c1b90a43dda2e389a6b2
Nathanael Seen <e0345677@u.nus.edu>
2020-04-11 17:12:33 +0800

    Complete requirement non existent tests for RequirementAssignCommand.java

--- src/test/java/igrad/logic/commands/requirement/RequirementAssignCommandTest.java
+++ src/test/java/igrad/logic/commands/requirement/RequirementAssignCommandTest.java
@@ -2,11 +2,15 @@ package igrad.logic.commands.requirement;
 
 //@@author nathanaelseen
 
+import static igrad.logic.commands.CommandTestUtil.assertExecuteFailure;
+// import static igrad.logic.commands.CommandTestUtil.assertExecuteSuccess;
 import static igrad.logic.commands.module.ModuleCommandTestUtil.VALID_MODULE_CODE_CS1101S;
 import static igrad.logic.commands.module.ModuleCommandTestUtil.VALID_MODULE_CODE_CS2040;
 import static igrad.logic.commands.module.ModuleCommandTestUtil.VALID_MODULE_CODE_CS2100;
+import static igrad.logic.commands.requirement.RequirementCommand.MESSAGE_REQUIREMENT_NON_EXISTENT;
 import static igrad.logic.commands.requirement.RequirementCommandTestUtil.VALID_REQ_CODE_UE;
 import static igrad.testutil.Assert.assertThrows;
+import static igrad.testutil.TypicalRequirements.GENERAL_ELECTIVES;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,10 +18,10 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 
 import igrad.model.Model;
+import igrad.model.ModelManager;
 import igrad.model.module.ModuleCode;
 import igrad.model.requirement.RequirementCode;
 // import igrad.model.requirement.Requirement;
-// import igrad.model.ModelManager;
 
 public class RequirementAssignCommandTest {
     @Test
@@ -57,5 +61,41 @@ public class RequirementAssignCommandTest {
 
         RequirementAssignCommand cmd = new RequirementAssignCommand(requirementCode, moduleCodes);
         assertThrows(NullPointerException.class, () -> cmd.execute(model));
+    }
+
+    @Test
+    public void execute_requirementNonExistentOnEmptyModel_failure() {
+        Model model = new ModelManager(); // set-up an empty Model
+        RequirementCode requirementCode = new RequirementCode(VALID_REQ_CODE_UE);
+        List<ModuleCode> moduleCodes = new ArrayList<ModuleCode>() {
+            {
+                add(new ModuleCode(VALID_MODULE_CODE_CS1101S));
+                add(new ModuleCode(VALID_MODULE_CODE_CS2040));
+                add(new ModuleCode(VALID_MODULE_CODE_CS2100));
+            }
+        };
+        RequirementAssignCommand cmd = new RequirementAssignCommand(requirementCode, moduleCodes);
+
+        assertExecuteFailure(cmd, model, MESSAGE_REQUIREMENT_NON_EXISTENT);
+    }
+
+    @Test
+    public void execute_requirementNonExistentNonEmptyModel_failure() {
+        // set-up a non-empty Model
+        Model model = new ModelManager();
+        model.addRequirement(GENERAL_ELECTIVES);
+
+        RequirementCode requirementCode = new RequirementCode(VALID_REQ_CODE_UE);
+        List<ModuleCode> moduleCodes = new ArrayList<ModuleCode>() {
+            {
+                add(new ModuleCode(VALID_MODULE_CODE_CS1101S));
+                add(new ModuleCode(VALID_MODULE_CODE_CS2040));
+                add(new ModuleCode(VALID_MODULE_CODE_CS2100));
+            }
+        };
+
+        RequirementAssignCommand cmd = new RequirementAssignCommand(requirementCode, moduleCodes);
+
+        assertExecuteFailure(cmd, model, MESSAGE_REQUIREMENT_NON_EXISTENT);
     }
 }
