e24e416eb8a9215eed0c032d28bf641dbda96d5b
NinoMii <e0323600@u.nus.edu>
2020-04-13 02:43:50 +0800

    Add some ug

--- src/main/java/igrad/logic/parser/Prefix.java
+++ src/main/java/igrad/logic/parser/Prefix.java
@@ -2,7 +2,7 @@ package igrad.logic.parser;
 
 /**
  * A prefix that marks the beginning of an argument in an arguments string.
- * E.g. 'n/' in 'module add n/CS1101S t/Programming Methodology I'.
+ * E.g. 'n/' in 'module add n/CS1101S t/Programming Methodology I u/4'.
  */
 public class Prefix {
     private final String prefix;
